1eeb5d46d00453ede58f708be99327286cae31e7
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-03-01 13:56:54 -0800

    added handlebars implementation

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -2,6 +2,7 @@
 
 const articles = [];
 
+
 function Article(rawDataObj) {
     this.author = rawDataObj.author;
     this.authorUrl = rawDataObj.authorUrl;
@@ -9,7 +10,6 @@ function Article(rawDataObj) {
     this.category = rawDataObj.category;
     this.body = rawDataObj.body;
     this.publishedOn = rawDataObj.publishedOn;
-   
     // REVIEW: If your template will use properties that aren't on the object yet, add them.
     // Since your template can't hold any JS logic, we need to execute the logic here.
     // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -19,11 +19,11 @@ function Article(rawDataObj) {
 }
 
 Article.prototype.toHtml = function () {
-    // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
-
+    // TODOne: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+    const template = $('#article-template').html();
+    const templateRender = Handlebars.compile(template);
     // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+    return templateRender(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
