fbbc05a7ea5282b1b1b4caf2846ac455af87d8d0
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-03-01 14:25:31 -0800

    updated .toHtml

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -9,7 +9,7 @@ function Article(rawDataObj) {
     this.category = rawDataObj.category;
     this.body = rawDataObj.body;
     this.publishedOn = rawDataObj.publishedOn;
-   
+
     // REVIEW: If your template will use properties that aren't on the object yet, add them.
     // Since your template can't hold any JS logic, we need to execute the logic here.
     // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -20,10 +20,11 @@ function Article(rawDataObj) {
 
 Article.prototype.toHtml = function () {
     // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
+    const templateElement = $('#article-template');
+    const template = Handlebars.compile(templateElement.html());
 
     // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+    return template(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
@@ -32,10 +33,16 @@ rawData.sort((a, b) => {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-rawData.forEach(articleObject => {
-    articles.push(new Article(articleObject));
-});
+// rawData.forEach(articleObject => {
+//     articles.push(new Article(articleObject));
+// });
 
-articles.forEach(article => {
-    $('#articles').append(article.toHtml());
+// articles.forEach(article => {
+//     $('#articles').append(article.toHtml());
+// });
+
+rawData.forEach(articleData => {
+    const newArticle = new Article(articleData);
+    const html = newArticle.toHtml();
+    $('#articles').append(html);
 });
