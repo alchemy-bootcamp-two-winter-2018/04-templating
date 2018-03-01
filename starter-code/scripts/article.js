2cacc728b57a19645579fa24d5b57d821bfd1bf0
Ryan Manro <rmanro@gmail.com>
2018-03-01 14:00:21 -0800

    added handlebars template to html

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -20,7 +20,7 @@ function Article(rawDataObj) {
 
 Article.prototype.toHtml = function () {
     // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
+    
 
     // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
