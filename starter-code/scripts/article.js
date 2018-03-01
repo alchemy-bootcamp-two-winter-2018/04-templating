864c8ff849f4d46961ff3b3e3aaad14e22f0b08b
Ryan Manro <rmanro@gmail.com>
2018-03-01 14:30:32 -0800

    getting most of the articles to show up on load

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -9,7 +9,7 @@ function Article(rawDataObj) {
     this.category = rawDataObj.category;
     this.body = rawDataObj.body;
     this.publishedOn = rawDataObj.publishedOn;
-   
+
     // REVIEW: If your template will use properties that aren't on the object yet, add them.
     // Since your template can't hold any JS logic, we need to execute the logic here.
     // The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -20,7 +20,10 @@ function Article(rawDataObj) {
 
 Article.prototype.toHtml = function () {
     // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-    
+    const hbTemplateTag = $('#article-template');
+    const hbTemplate = Handlebars.compile(hbTemplateTag.html());
+    const hbInstance = hbTemplate(this);
+    return hbInstance;
 
     // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
